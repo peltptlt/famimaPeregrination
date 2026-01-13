ca0bc441f7a76f6124fcaf3cc9f29af6576d7b51
rohe <peltftrt@yahoo.co.jp>
2026-01-13 23:02:18 +0900

    reset

--- docs/map.js
+++ docs/map.js
@@ -1,24 +1,10 @@
-window.map = new maplibregl.Map({
+const map = new maplibregl.Map({
   container: 'map',
-  center: [135.7, 35.0],
-  zoom: 6,
-  style: {
-    version: 8,
-    glyphs: 'https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf',
-    sources: {
-      osm: {
-        type: 'raster',
-        tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
-        tileSize: 256
-      }
-    },
-    layers: [{
-      id: 'osm',
-      type: 'raster',
-      source: 'osm'
-    }]
-  }
+  style: 'https://demotiles.maplibre.org/style.json',
+  center: [139.7, 35.6],
+  zoom: 5
 });
+
 window.addEventListener("load", () => {
   map.resize();
 });
