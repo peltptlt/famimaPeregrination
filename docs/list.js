a5b5c78e112ba35a02d1c870ef6f1ab45a662d9c
rohe <peltftrt@yahoo.co.jp>
2026-05-10 22:53:43 +0900

    Update list.js

--- docs/list.js
+++ docs/list.js
@@ -30,7 +30,8 @@ const maxPrefLen = Math.max(...Object.keys(prefCounts).map(p => p.length));
 const maxCityLen = Math.max(...Object.keys(cityCounts).map(c => c.length));
 
 function padZenkaku(str, width) {
-  return str + "  ".repeat(width - str.length);
+  const padCount = width - str.length;
+  return str + "　".repeat(padCount > 0 ? padCount : 0);
 }
 
 // 都道府県フィルター生成
